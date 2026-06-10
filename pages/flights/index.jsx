2754ea940621ddbd0670c67f7a97451e0d212235
payplex10 <priyankasangamkar@payplex.org.in>
2026-06-10 20:05:21 +0530

    bookfilght page ui

--- pages/flights/index.jsx
+++ pages/flights/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FlightSearch from '../../src/components/flights/FlightSearch'
+import BookFlight from "../../src/components/flights/BookFlight";
 
 const index = () => {
   return (
     <>
-    <FlightSearch/>
+      <FlightSearch/>
+      <BookFlight/>
     </>
   )
 }
